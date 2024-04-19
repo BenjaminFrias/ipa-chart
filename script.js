27c6586f41602807fd64c91c490872cb37918b43
BenjaminFrias <benjaminfrias2004@gmail.com>
2024-04-19 15:11:58 -0400

    add ham menu

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const arrowBtn = document.querySelector(".arrow-btn");
 const ipaChart = document.querySelector(".ipa-chart-container");
 const ipaLetters = document.querySelectorAll(".ipa-item span");
+const hamMenu = document.querySelector(".ham-menu");
+const nav = document.querySelector("header nav");
+const closeBtn = document.querySelector(".close-btn");
+const navLinks = nav.querySelectorAll("ul li a");
 
-ipaLetters.forEach((letter) => {
-    letter.addEventListener("click", () => {
-        const audioName = letter.dataset.audio;
-        var audio = new Audio(`assets/audio/${audioName}.mp3`);
-        audio.play();
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        if (nav.classList.contains("active")) {
+            nav.classList.toggle("active");
+        }
     });
+});
+
+closeBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+hamMenu.addEventListener("click", () => {
+    nav.classList.toggle("active");
 });
 
 arrowBtn.addEventListener("click", () => {
@@ -30,3 +42,11 @@ function findPosition(obj) {
         return [currentTop];
     }
 }
+
+ipaLetters.forEach((letter) => {
+    letter.addEventListener("click", () => {
+        const audioName = letter.dataset.audio;
+        var audio = new Audio(`assets/audio/${audioName}.mp3`);
+        audio.play();
+    });
+});
